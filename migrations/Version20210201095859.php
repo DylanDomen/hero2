3680881b8b18d586056ee2e7db4656c59d49690e
Dylan <dylan.domen@gmail.com>
2021-02-01 13:59:26 +0400

    frff

--- migrations/Version20210201095859.php
+++ migrations/Version20210201095859.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210201094215 extends AbstractMigration
+final class Version20210201095859 extends AbstractMigration
 {
     public function getDescription() : string
     {
